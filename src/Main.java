0717e8cd38d34f7f5f977210ea0f3f251a619e9d
Erick0105 <erickspot01@gmail.com>
2024-04-14 21:28:22 -0300

    privatização de atributos

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,20 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner leitor = new Scanner(System.in);
+        Triangulo triangulo = new Triangulo();
 
         System.out.println("Qual o valor da base do triângulo?");
-        double base = leitor.nextDouble();
+        triangulo.setBase(leitor.nextDouble());
         System.out.println("E qual o valor da altura deste triângulo?");
-        double altura = leitor.nextDouble();
+        triangulo.setAltura(leitor.nextDouble());
+        System.out.println("Qual o valor do lado sem ser a base?");
+        triangulo.setLado2(leitor.nextDouble());
+        System.out.println("Qual o valor do outro lado?");
+        triangulo.setLado3(leitor.nextDouble());
 
-        Triangulo triangulo = new Triangulo(base,altura);
-
-        System.out.println(triangulo.area());
+        System.out.println("------------------------------------------\n" +
+                "Área do triângulo: " + triangulo.area_triangulo() +
+                "\nPérimetro do triângulo: " + triangulo.perimetro_triangulo() +
+                "\n------------------------------------------");
     }
 }
--- src/Triangulo.java
+++ src/Triangulo.java
@@ -1,19 +1,46 @@
 public class Triangulo {
 
-    public double base;
-    public double altura;
-    public double lado2;
-    public double lado3;
+    private double base;
+    private double altura;
+    private double lado2;
+    private double lado3;
 
-        public Triangulo(double base, double altura){
-            this.base = base;
-            this.altura = altura;
-    }
-        public double area(){
-            return base * altura / 2;
-        }
-        public double perimetro(){
-            return base + lado2 + lado3;
-        }
+    public double getBase() {
+        return base;
     }
 
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getLado2() {
+        return lado2;
+    }
+
+    public void setLado2(double lado2) {
+        this.lado2 = lado2;
+    }
+
+    public double getLado3() {
+        return lado3;
+    }
+
+    public void setLado3(double lado3) {
+        this.lado3 = lado3;
+    }
+
+    public double area_triangulo(){
+        return (getBase() * getAltura())/2;
+    }
+    public double perimetro_triangulo(){
+        return getBase() + getLado2() + getLado3();
+    }
+}
